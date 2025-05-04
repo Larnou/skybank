a7208bfd5c54c96e56056b90ada62ce5efbb915c
Larnou <sfas921@yandex.ru>
2025-05-04 22:35:35 +0800

    Добавлен модуль widget.py

--- src/masks.py
+++ src/masks.py
@@ -1,22 +1,20 @@
-def get_mask_card_number(card_number: int) -> str:
+def get_mask_card_number(card_number: str) -> str:
     """
     Функция get_mask_card_number принимает на вход номер карты и возвращает ее маску. Номер карты замаскирован
     и отображается в формате XXXX XX** **** XXXX, где X — это цифра номера.
     :param card_number:
     :return:
     """
-    card_str = str(card_number)
-    masked_number = f"{card_str[:4]} {card_str[4:6]}** **** {card_str[-4:]}"
+    masked_number = f"{card_number[:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
     return masked_number
 
 
-def get_mask_account(account_number: int) -> str:
+def get_mask_account(account_number: str) -> str:
     """
     Функция get_mask_account принимает на вход номер счета и возвращает его маску. Номер счета замаскирован
     и отображается в формате **XXXX, где X — это цифра номера.
     :param account_number:
     :return:
     """
-    account_str = str(account_number)
-    masked_number = f"**{account_str[-4:]}"
+    masked_number = f"{account_number[:4]} **{account_number[-4:]}"
     return masked_number
---
+++ src/widget.py
@@ -0,0 +1,31 @@
+from datetime import datetime
+from masks import get_mask_card_number, get_mask_account
+
+
+def mask_account_card(account_card: str) -> str:
+    """
+    Функция mask_account_card принимает на вход номер карты или счета и возвращает ее маску.
+    :param account_card:
+    :return:
+    """
+    if account_card[:4].lower() == 'счет':
+        masked_number = get_mask_account(account_card)
+    else:
+        masked_number = get_mask_card_number(account_card)
+
+    return masked_number
+
+def get_date(date: str) -> str:
+    """
+    Функция get_date принимает на вход дату в формате ISO8601 '%Y-%m-%dT%H:%M:%S.ffffff' и возвращает
+    в формате '%d.%m.%Y'.
+    :param date:
+    :return:
+    """
+    # Time-форматы
+    date_format = '%d.%m.%Y'
+
+    date_isoformat = datetime.fromisoformat(date)
+    formated_date = datetime.strftime(date_isoformat, date_format)
+
+    return str(formated_date)
