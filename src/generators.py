7a0a48693f2c9247b079167a7badefe352804ac6
Larnou <sfas921@yandex.ru>
2025-05-30 12:21:11 +0800

    # 111_3: Обнавлён файл README.md, добавлены результаты о покрытии тестами с учётом нового модуля

--- src/generators.py
+++ src/generators.py
@@ -1,5 +1,6 @@
 from typing import Iterator
 
+
 def filter_by_currency(transactions_list: list[dict], currency: str) -> Iterator[dict]:
     """
     Принимает на вход список словарей, представляющих транзакции, возвращать итератор, который поочередно
@@ -8,18 +9,25 @@ def filter_by_currency(transactions_list: list[dict], currency: str) -> Iterator
     :param currency: Валюта, по которой необходимо вывести транзакцию
     :return:
     """
-    for transaction in transactions_list:
-        if transaction.get('operationAmount').get('currency').get('code') == currency:
-            yield transaction
+    for i, transaction in enumerate(transactions_list, 1):
+        try:
+            if transaction["operationAmount"]["currency"]["code"] == currency:
+                yield transaction
+        except Exception as e:
+            raise ValueError(f"Транзакция #{i} отсутствует поле: {e}. Данные: {transaction}") from e
 
-def transaction_descriptions(transactions_list: list[dict]) -> Iterator[str]:
+
+def transaction_descriptions(transactions_list: list[dict]) -> Iterator[str | None]:
     """
     Принимает список словарей с транзакциями и возвращает описание каждой операции по очереди.
     :param transactions_list: Список словарей с информацией по транзакциям.
     :return:
     """
-    for transaction in transactions_list:
-        yield transaction.get('description')
+    for i, transaction in enumerate(transactions_list, 1):
+        try:
+            yield transaction.get("description")
+        except Exception as e:
+            raise ValueError(f"Транзакция #{i} отсутствует поле: {e}. Данные: {transaction}") from e
 
 
 def card_number_generator(start: int, stop: int) -> Iterator[str]:
@@ -27,13 +35,13 @@ def card_number_generator(start: int, stop: int) -> Iterator[str]:
     Выдает номера банковских карт в формате XXXX XXXX XXXX XXXX, где X — цифра номера карты.
     Генератор может сгенерировать номера карт в заданном диапазоне от 0000 0000 0000 0001 до 9999 9999 9999 9999.
     :param start: Минимальное значение номера карты.
-    :param end: Максимальное значение номера карты.
+    :param stop: Максимальное значение номера карты.
     :return:
     """
     if start > 0 and stop <= 9999999999999999:
         for i in range(start, stop + 1):
             card_number = str(i).zfill(16)
-            groups = [card_number[i:i + 4] for i in range(0, 16, 4)]
+            groups = [card_number[i : i + 4] for i in range(0, 16, 4)]
             yield " ".join(groups)
     else:
         raise ValueError("Проверьте правильность введённых границ номера карты.")
--- tests/test_generators.py
+++ tests/test_generators.py
@@ -1,18 +1,22 @@
 import pytest
 
-from src.generators import filter_by_currency, transaction_descriptions, card_number_generator
+from src.generators import card_number_generator, filter_by_currency, transaction_descriptions
 
 # Тестирование генератора card_number_generator:
 # Напишите тесты, которые проверяют, что генератор выдает правильные номера карт в заданном диапазоне.
 # Проверьте корректность форматирования номеров карт.
 # Убедитесь, что генератор корректно обрабатывает крайние значения диапазона и правильно завершает генерацию.
 
-@pytest.mark.parametrize("card_number_start, card_number_stop, card_number_result", [
-    (1, 2, ['0000 0000 0000 0001', '0000 0000 0000 0002']),
-    (23456, 23457, ['0000 0000 0002 3456', '0000 0000 0002 3457']),
-    (12345678, 12345679, ['0000 0000 1234 5678', '0000 0000 1234 5679']),
-    (9999999999999998, 9999999999999999, ['9999 9999 9999 9998', '9999 9999 9999 9999'])
-])
+
+@pytest.mark.parametrize(
+    "card_number_start, card_number_stop, card_number_result",
+    [
+        (1, 2, ["0000 0000 0000 0001", "0000 0000 0000 0002"]),
+        (23456, 23457, ["0000 0000 0002 3456", "0000 0000 0002 3457"]),
+        (12345678, 12345679, ["0000 0000 1234 5678", "0000 0000 1234 5679"]),
+        (9999999999999998, 9999999999999999, ["9999 9999 9999 9998", "9999 9999 9999 9999"]),
+    ],
+)
 def test_card_number_generator(card_number_start, card_number_stop, card_number_result):
     result = list(card_number_generator(card_number_start, card_number_stop))
     assert result == card_number_result
@@ -27,7 +31,7 @@ def test_card_number_generator_bad_start_value():
             results.append(item)
 
     assert results == []
-    assert str(exc_info.value) == 'Проверьте правильность введённых границ номера карты.'
+    assert str(exc_info.value) == "Проверьте правильность введённых границ номера карты."
 
 
 def test_card_number_generator_bad_stop_value():
@@ -39,47 +43,52 @@ def test_card_number_generator_bad_stop_value():
             results.append(item)
 
     assert results == []
-    assert str(exc_info.value) == 'Проверьте правильность введённых границ номера карты.'
+    assert str(exc_info.value) == "Проверьте правильность введённых границ номера карты."
 
 
 # Тестирование функции transaction_descriptions:
 # Проверьте, что функция возвращает корректные описания для каждой транзакции.
 # Тестируйте работу функции с различным количеством входных транзакций, включая пустой список.
 
+
 def test_transaction_descriptions(list_of_transactions):
     generator_result = [x for x in transaction_descriptions(list_of_transactions)]
     assert generator_result == [
-        'Перевод организации',
-        'Перевод со счета на счет',
-        'Перевод со счета на счет',
-        'Перевод с карты на карту',
-        'Перевод организации'
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
     ]
+
 
 def test_transaction_descriptions_doubled(double_list_of_transactions):
     generator_result = [x for x in transaction_descriptions(double_list_of_transactions)]
     assert generator_result == [
-        'Перевод организации',
-        'Перевод со счета на счет',
-        'Перевод со счета на счет',
-        'Перевод с карты на карту',
-        'Перевод организации',
-        'Перевод организации',
-        'Перевод со счета на счет',
-        'Перевод со счета на счет',
-        'Перевод с карты на карту',
-        'Перевод организации'
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
+        "Перевод организации",
+        "Перевод со счета на счет",
+        "Перевод со счета на счет",
+        "Перевод с карты на карту",
+        "Перевод организации",
     ]
+
 
 def test_transaction_descriptions_empty_list(empty_list_of_transactions):
     generator_result = [x for x in transaction_descriptions(empty_list_of_transactions)]
     assert generator_result == []
+
 
 # Тестирование функции filter_by_currency:
 # Напишите тесты, проверяющие, что функция корректно фильтрует транзакции по заданной валюте.
 # Проверьте, что функция правильно обрабатывает случаи, когда транзакции в заданной валюте отсутствуют.
 # Убедитесь, что генератор не завершается ошибкой при обработке пустого списка или списка без соответствующих
 # валютных операций.
+
 
 def test_filter_by_currency_usd(list_of_transactions):
     generator_result = [x for x in filter_by_currency(list_of_transactions, "USD")]
@@ -88,48 +97,31 @@ def test_filter_by_currency_usd(list_of_transactions):
             "id": 939719570,
             "state": "EXECUTED",
             "date": "2018-06-30T02:08:58.425572",
-            "operationAmount": {
-                "amount": "9824.07",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "9824.07", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод организации",
             "from": "Счет 75106830613657916952",
-            "to": "Счет 11776614605963066702"
+            "to": "Счет 11776614605963066702",
         },
         {
             "id": 142264268,
             "state": "EXECUTED",
             "date": "2019-04-04T23:20:05.206878",
-            "operationAmount": {
-                "amount": "79114.93",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "79114.93", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 19708645243227258542",
-            "to": "Счет 75651667383060284188"
+            "to": "Счет 75651667383060284188",
         },
         {
             "id": 895315941,
             "state": "EXECUTED",
             "date": "2018-08-19T04:27:37.904916",
-            "operationAmount": {
-                "amount": "56883.54",
-                "currency": {
-                    "name": "USD",
-                    "code": "USD"
-                }
-            },
+            "operationAmount": {"amount": "56883.54", "currency": {"name": "USD", "code": "USD"}},
             "description": "Перевод с карты на карту",
             "from": "Visa Classic 6831982476737658",
-            "to": "Visa Platinum 8990922113665229"
+            "to": "Visa Platinum 8990922113665229",
         },
     ]
+
 
 def test_filter_by_currency_rub(list_of_transactions):
     generator_result = [x for x in filter_by_currency(list_of_transactions, "RUB")]
@@ -138,33 +130,22 @@ def test_filter_by_currency_rub(list_of_transactions):
             "id": 873106923,
             "state": "EXECUTED",
             "date": "2019-03-23T01:09:46.296404",
-            "operationAmount": {
-                "amount": "43318.34",
-                "currency": {
-                    "name": "руб.",
-                    "code": "RUB"
-                }
-            },
+            "operationAmount": {"amount": "43318.34", "currency": {"name": "руб.", "code": "RUB"}},
             "description": "Перевод со счета на счет",
             "from": "Счет 44812258784861134719",
-            "to": "Счет 74489636417521191160"
+            "to": "Счет 74489636417521191160",
         },
         {
-                    "id": 594226727,
-                    "state": "CANCELED",
-                    "date": "2018-09-12T21:27:25.241689",
-                    "operationAmount": {
-                        "amount": "67314.70",
-                        "currency": {
-                            "name": "руб.",
-                            "code": "RUB"
-                        }
-                    },
-                    "description": "Перевод организации",
-                    "from": "Visa Platinum 1246377376343588",
-                    "to": "Счет 14211924144426031657"
-        }
+            "id": 594226727,
+            "state": "CANCELED",
+            "date": "2018-09-12T21:27:25.241689",
+            "operationAmount": {"amount": "67314.70", "currency": {"name": "руб.", "code": "RUB"}},
+            "description": "Перевод организации",
+            "from": "Visa Platinum 1246377376343588",
+            "to": "Счет 14211924144426031657",
+        },
     ]
+
 
 def test_filter_by_currency_empty_list(empty_list_of_transactions):
     generator_result = [x for x in filter_by_currency(empty_list_of_transactions, "RUB")]
