d536a1721052d47efb84e4845ae25f954c1dfeb9
Larnou <sfas921@yandex.ru>
2025-06-02 23:26:44 +0800

    # 121_2: Реализация работы функции convert_transactions_to_rub()

--- src/external_api.py
+++ src/external_api.py
@@ -1,29 +1,43 @@
+import json
 
+import requests
 
 from src.utils import read_json
 import os
 from dotenv import load_dotenv
 
 operations = read_json('../data/operations.json')
-# Загрузка переменных из .env-файла
-load_dotenv()
 
-# Получение значения переменной GITHUB_TOKEN из .env-файла
-github_token = os.getenv('GITHUB_TOKEN')
 
-def convert_transactions_tu_rub(transaction: dict) -> float:
+def convert_transactions_to_rub(transaction: dict):
+    load_dotenv()
+    API_LAYER_KEY = os.getenv('API_LAYER_KEY')
 
     try:
-         currency = transaction.get('operationAmount').get('currency').get('code')
+        from_currency = transaction.get('operationAmount').get('currency').get('code')
+        to_currency = 'RUB'
+        amount = transaction.get('operationAmount').get('amount')
+
+        if from_currency == to_currency:
+            return amount
+        else:
+
+            payload = {}
+            headers = {
+                "apikey": API_LAYER_KEY
+            }
+
+            url = f'https://api.apilayer.com/exchangerates_data/convert?to={to_currency}&from={from_currency}&amount={amount}'
+            response = requests.request("GET", url, headers=headers, data=payload)
+
+            parsed_result = json.loads(response.text)
+            parsed_amount = parsed_result.get('result')
+
+        return parsed_amount
+
     except Exception as e:
         raise ValueError(f"В транзакции отсутствует поле: {e}. Данные: {transaction}") from e
 
-    return currency
 
-
-
-
-
-result = convert_transactions_tu_rub(operations[1])
+result = convert_transactions_to_rub(operations[0])
 print(f'{result} рублей')
-print(github_token)
