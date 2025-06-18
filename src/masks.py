f5a4e8f4cc4d9277dfbbc4e14889b0c22843c339
Larnou <sfas921@yandex.ru>
2025-06-18 22:40:16 +0800

    # 122_3: справлены ошибки во время проверки линтерами

--- src/masks.py
+++ src/masks.py
@@ -1,7 +1,7 @@
 from src.utils import create_logger
 
+masks_logger = create_logger("masks_logger", "masks")
 
-masks_logger = create_logger('masks_logger', 'masks')
 
 def get_mask_card_number(card_number: str) -> str:
     """
@@ -13,15 +13,15 @@ def get_mask_card_number(card_number: str) -> str:
 
     card_digits_part = card_number.split(" ")[-1]
     digits_count = len([char.isdigit() for char in card_digits_part if char.isdigit()])
-    masks_logger.info(f'Получен следующий номер карты {card_digits_part}.')
+    masks_logger.info(f"Получен следующий номер карты {card_digits_part}.")
 
     if digits_count == 16 and len(card_digits_part) == 16:
-        masks_logger.info(f'Номер карты {card_digits_part} введён правильно.')
+        masks_logger.info(f"Номер карты {card_digits_part} введён правильно.")
         masked_number = f"{card_number[:-12]} {card_number[-12:-10]}** **** {card_number[-4:]}"
-        masks_logger.info(f'Результат маскирования {masked_number}.')
+        masks_logger.info(f"Результат маскирования {masked_number}.")
         return masked_number
     else:
-        error_message = 'Проверьте правильность введённого номера карты.'
+        error_message = "Проверьте правильность введённого номера карты."
         masks_logger.error(error_message)
         raise ValueError(error_message)
 
@@ -36,12 +36,12 @@ def get_mask_account(account_number: str) -> str:
 
     card_digits_part = account_number.split(" ")[-1]
     digits_count = len([char.isdigit() for char in card_digits_part if char.isdigit()])
-    masks_logger.info(f'Получен следующий номер счёта {account_number}.')
+    masks_logger.info(f"Получен следующий номер счёта {account_number}.")
 
     if digits_count == 20 and len(card_digits_part) == 20:
-        masks_logger.info(f'Номер счёта {card_digits_part} введён правильно.')
+        masks_logger.info(f"Номер счёта {card_digits_part} введён правильно.")
         masked_number = f"{account_number[:4]} **{account_number[-4:]}"
-        masks_logger.info(f'Результат маскирования {masked_number}.')
+        masks_logger.info(f"Результат маскирования {masked_number}.")
         return masked_number
     else:
         error_message = "Проверьте правильность введённого номера счёта."
--- src/utils.py
+++ src/utils.py
@@ -14,18 +14,20 @@ def create_logger(logger_name, filename) -> logging.Logger:
     logger.setLevel(logging.DEBUG)
 
     # Основной файл (все сообщения)
-    main_handler = logging.FileHandler(filename=f"../logs/{filename}.log", mode='w', encoding='utf-8')
+    main_handler = logging.FileHandler(filename=f"../logs/{filename}.log", mode="w", encoding="utf-8")
 
     # Настройка форматтеров
-    main_handler.setFormatter(logging.Formatter(
-        "[%(levelname)s] %(asctime)s - module %(filename)s in %(funcName)s: %(message)s"
-    ))
+    main_handler.setFormatter(
+        logging.Formatter("[%(levelname)s] %(asctime)s - module %(filename)s in %(funcName)s: %(message)s")
+    )
 
     # Добавление обработчиков
     logger.addHandler(main_handler)
     return logger
 
-utils_logger = create_logger('utils_logger', 'utils')
+
+utils_logger = create_logger("utils_logger", "utils")
+
 
 def read_json(filepath: str) -> list[Any] | Any:
     """
@@ -34,12 +36,12 @@ def read_json(filepath: str) -> list[Any] | Any:
     :return: Данные файла JSON или пустой список, если произошла ошибка чтения.
     """
     try:
-        utils_logger.info(f'Попытка открыть файл {filepath}.')
+        utils_logger.info(f"Попытка открыть файл {filepath}.")
         with open(filepath, encoding="utf8") as f:
             data = json.load(f)
-        utils_logger.info('Данные файла успешно прочитаны')
+        utils_logger.info("Данные файла успешно прочитаны")
 
         return data
     except (FileNotFoundError, json.JSONDecodeError, UnicodeDecodeError, PermissionError, IsADirectoryError):
-        utils_logger.error('При чтении данных возникла ошибка')
+        utils_logger.error("При чтении данных возникла ошибка")
         return []
