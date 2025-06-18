222ab0da5f97e56a32dba0abb17aeace128fc920
Larnou <sfas921@yandex.ru>
2025-06-18 22:29:57 +0800

    # 122_1: Добавление логирования для модуля masks.py

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,5 @@
 import json
+import logging
 from typing import Any
 
 
@@ -15,3 +16,26 @@ def read_json(filepath: str) -> list[Any] | Any:
         return data
     except (FileNotFoundError, json.JSONDecodeError, UnicodeDecodeError, PermissionError, IsADirectoryError):
         return []
+
+
+def create_logger(logger_name, filename):
+    """
+    Создание именованного логера с записью в определенёый файл.
+    :param logger_name: Название именованного логера.
+    :param filename: Названия файла, в который будут записаны результаты логирования.
+    :return: Именованный логер logger_name
+    """
+    logger = logging.getLogger(logger_name)
+    logger.setLevel(logging.DEBUG)
+
+    # Основной файл (все сообщения)
+    main_handler = logging.FileHandler(filename=f"../logs/{filename}.log", mode='w', encoding='utf-8')
+
+    # Настройка форматтеров
+    main_handler.setFormatter(logging.Formatter(
+        "[%(levelname)s] %(asctime)s - module %(filename)s in %(funcName)s: %(message)s"
+    ))
+
+    # Добавление обработчиков
+    logger.addHandler(main_handler)
+    return logger
