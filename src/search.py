8ba4c053903d98916ad803c44b43721773577e6c
Larnou <sfas921@yandex.ru>
2025-06-19 23:08:41 +0800

    # 132_2: Обновлён файл README.md, добавлено описание функций модуля search.py

--- src/search.py
+++ src/search.py
@@ -5,9 +5,19 @@ from src.parser import read_file_from_csv
 
 # data = read_file_from_csv('../data/transactions.csv')
 
-def process_bank_search(data:list[dict], search_line:str) -> list[dict]:
+def process_bank_search(data:list[dict], search_string:str) -> list[dict]:
+    """
+        Фильтрует список банковских операций по наличию строки в описании.
+
+        Args:
+            data: Список словарей с данными операций
+            search_string: Строка для поиска в описании операций
+
+        Returns:
+            Отфильтрованный список операций, где строка найдена в описании
+        """
     # Компилируем регулярное выражение с флагом IGNORECASE
-    pattern = re.compile(re.escape(search_line), re.IGNORECASE)
+    pattern = re.compile(re.escape(search_string), re.IGNORECASE)
 
     # Фильтруем операции, где описание соответствует шаблону
     result = [operation for operation in data if pattern.search(operation.get("description"))]
